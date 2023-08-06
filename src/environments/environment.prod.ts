75f366deb8d731fcdbf92a9a522338b67f35849e
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-08-06 16:06:01 +0300

    add config for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://http://delivery-app.us-east-1.elasticbeanstalk.com/'
+  apiUrl: 'http://delivery-app.us-east-1.elasticbeanstalk.com/'
 };
