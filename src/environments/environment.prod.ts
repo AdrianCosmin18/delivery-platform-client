ec7552802a31b0a8fa281e426af3791e870c4350
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-08-19 14:11:58 +0300

    modify environment prod path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://delivery-app.us-east-1.elasticbeanstalk.com/'
+  apiUrl: 'http://delivery-app.us-east-1.elasticbeanstalk.com/delivery-app/'
 };
